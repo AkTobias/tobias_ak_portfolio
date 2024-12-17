93c8c4fa412b96bfac236d3728d1d3949ee43a74
Aktobias <tobias.gh.akermark@gmail.com>
2024-12-17 14:52:02 +0100

    basic navbar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,11 +4,17 @@ function Header() {
    return (
       <header className="header">
          <nav>
-            <ul>
+            <ul className="header__ul">
                <li>
                   <Link to="/">Home</Link>
+               </li>
+               <li>
                   <Link to="/about">About</Link>
+               </li>
+               <li>
                   <Link to="/portfolio">Portfolio</Link>
+               </li>
+               <li>
                   <Link to="/contact">Contact</Link>
                </li>
             </ul>
