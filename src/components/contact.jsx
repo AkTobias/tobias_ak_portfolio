1626a537095e4c4ea70a1e7d5750aded210a0718
Aktobias <tobias.gh.akermark@gmail.com>
2024-12-17 19:22:45 +0100

    contact component, not finished

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,11 +1,22 @@
 function Contact() {
    return (
-      <section>
-         <h2>Contact me</h2>
-         <p>Email: tobias.test@test.com</p>
-         <p>
-            LinkedIn <a href="https://linkedin.com"></a>
-         </p>
+      <section className="contact__section">
+         <form className="contact__form" action="">
+            <h2>Contact me </h2>
+            <h3>Name </h3>
+
+            <input type="text" />
+            <h3>Email </h3>
+
+            <input type="text" />
+            <h3>Subject</h3>
+
+            <input type="text" />
+
+            <h3>Message</h3>
+            <textarea name="text" id="" cols={40} rows={10}></textarea>
+            <button type="submit">Send me an email</button>
+         </form>
       </section>
    );
 }
