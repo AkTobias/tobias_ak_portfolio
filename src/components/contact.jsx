3ab470162909480af58d5100cbd01d1e9380d744
Aktobias <tobias.gh.akermark@gmail.com>
2024-12-18 19:37:24 +0100

    removed old contact function

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -51,28 +51,4 @@ function Contact() {
    );
 }
 
-// old
-/* function Contact() {
-   return (
-      <section className="contact__section">
-         <form className="contact__form" action="">
-            <h2>Contact me </h2>
-            <h3>Name </h3>
-
-            <input type="text" />
-            <h3>Email </h3>
-
-            <input type="text" />
-            <h3>Subject</h3>
-
-            <input type="text" />
-
-            <h3>Message</h3>
-            <textarea name="text" id="" cols={40} rows={10}></textarea>
-            <button type="submit">Send me an email</button>
-         </form>
-      </section>
-   );
-} */
-
 export default Contact;
