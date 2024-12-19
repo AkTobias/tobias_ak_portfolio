033b0c68dfdbcaa705108bba4827edab699e916b
AkTobias <tobias.gh.akermark@gmail.com>
2024-12-19 11:50:52 +0100

    Not happy with the desing, gonna try something else maybe flex...

--- src/components/referrals.jsx
+++ src/components/referrals.jsx
@@ -6,7 +6,7 @@ function Referrals() {
       <section>
          <div className="card__container">
             {quotes.map((item, index)=> (
-               <div key={index} className={`card__box ${index % 2 === 0 ? "align-rigth" : "align-left"}`}>
+               <div key={index} className={`card__box ${index % 2 === 0 ? "align-right" : "align-left"}`}>
                   <p className="card__quote">"{item.quote}"</p>
                   <h2 className="card__name">-{item.name}</h2>
                </div>
